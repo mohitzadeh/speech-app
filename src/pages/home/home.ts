8a104c81f9d5228fc0647a05393ec60f2d5d8366
mohitzadeh <mohitzadeh@gmail.com>
2018-04-11 13:41:37 -0700

    checkin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TestPage } from '../test/test';
+import { Platform } from 'ionic-angular';
+//import { StatusBar, Splashscreen } from 'ionic-native';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  gototest(){
+    this.navCtrl.push(TestPage)
+  }
 }
